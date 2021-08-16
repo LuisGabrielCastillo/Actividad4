64d1d1cd36891cd1c2daac49817f752269f10968
LuisGabrielCastillo <89027056+LuisGabrielCastillo@users.noreply.github.com>
2021-08-16 08:50:33 -0700

    Update exercise.py

--- assignments/01AreayVolumen/src/exercise.py
+++ assignments/01AreayVolumen/src/exercise.py
@@ -1,7 +1,12 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    rad=float(input("Ingrese radio "))
+    from math import pi 
+    area=4*pi*rad**2
+    vol=(4*pi*rad**3)/3
+    print("El área de tu cilindro es",area)
+    print("El volumen de tu cilindro es",vol)
 
 
 
