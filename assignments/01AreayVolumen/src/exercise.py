f1e1f42141a0ac97c1afa62c9af2500a43f58cb2
LuisGabrielCastillo <89027056+LuisGabrielCastillo@users.noreply.github.com>
2021-08-17 13:04:29 -0700

    Update exercise.py

--- assignments/01AreayVolumen/src/exercise.py
+++ assignments/01AreayVolumen/src/exercise.py
@@ -1,9 +1,12 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
-
+    rad=float(input("Dame radio "))
+    from math import pi 
+    area=4*pi*rad**2
+    vol=(4*pi*rad**3)/3
+    print("Area:",area)
+    print("Volumen:",vol)
+    
 if __name__ == '__main__':
     main()
