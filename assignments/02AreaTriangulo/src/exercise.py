2e47c7f8cf50dbdb1b9b3e1979d2114105fca639
LuisGabrielCastillo <89027056+LuisGabrielCastillo@users.noreply.github.com>
2021-08-17 13:28:07 -0700

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,7 +1,13 @@
 import math
 def main():
      #escribe tu código abajo de esta línea
-   pass
+      la=float(input("Dame el valor a "))
+     lb=float(input("Dame el valor b "))
+     lc=float(input("Dame el valor c "))
+     s=(lb+la+lc)/2
+     from math import sqrt
+     area=sqrt((s-la)*(s-lb)*(s-lc))
+     print("Area:",area)
 
 
 if __name__ == '__main__':
